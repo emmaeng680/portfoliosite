039ef5656999600a60d896eb16af2afbbea364b1
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2024-11-27 03:23:26 -0600

    growing
    growing

---
+++ emmas-portfolio/components/Stairs.jsx
@@ -0,0 +1,17 @@
+import {motion} from "motion/react";
+
+//variants
+const stairAnimation = {
+    initial: {
+        
+    }
+}
+const Stairs = () => {
+    return (
+        <div>
+            stairs
+        </div>
+    );
+};
+
+export default Stairs;
