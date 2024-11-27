291c4bf44550d42cef3671390cfeb881b152422e
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2024-11-27 06:00:43 -0600

    updated
    more updates

--- emmas-portfolio/app/page.jsx
+++ emmas-portfolio/app/page.jsx
@@ -5,8 +5,19 @@ const Home = () => {
     return (
         <section className="h-full">
           <div className="container mx-auto h-full">
-            <div>
-                <div></div>
+            <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+                {/* text */}
+                <div className="text-center xl:text-left">
+                    <span className="text-xl">Software Developer</span>
+                    <h1 className="h1">
+                        Hello I'm <br/> <span> Emmanuel Oppong</span>
+                    </h1>
+                    <p>
+                        I excel at crafting elegant 
+                    </p>
+                </div>
+                {/* photo */}
+                <div>photo</div>
             </div>
           </div>
         </section>
